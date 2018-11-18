0dcc249507230b574a70428badedd5ff19bbac00
Marzena <marzena.szopinska090@gmail.com>
2018-11-18 13:41:27 +0100

    fixed an error inside the console regarding isOpen attribute inside Modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-    <Modal isOpen={props.selectedOption} 
+    <Modal isOpen={!!props.selectedOption} 
         contentLabel="Selected Option"
         onRequestClose={props.handleClearSelectedOption}
     >
